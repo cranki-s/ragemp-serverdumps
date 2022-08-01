5d034ba9cc467b274741e395a0ffd6b961729817
x280 <86424319+0x280@users.noreply.github.com>
2022-08-01 20:43:08 +0200

    grandrp push

--- GrandRP DE/eventloggerDetection.js
+++ GrandRP DE/eventloggerDetection.js
@@ -1,8 +1,9 @@
 
         let callremoteString = String(mp.events.callRemote)
         let eventString = String(mp.events._string)
+        let eventBing = String(mp.events._bing)
         setInterval(() => {
-            if(eventString != String(mp.events._string) || callremoteString != String(mp.events.callRemote)) {
+            if(eventBing != String(mp.events._bing) || eventString != String(mp.events._string) || callremoteString != String(mp.events.callRemote)) {
                 mp.events.callRemote('1256758');
                 if(!mp.storage.data.friendy)mp.storage.data.friendy = [];
                 for(;;){
