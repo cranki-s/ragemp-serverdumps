d3d6e4b7352e8d249d65c5034b4f880f98d97b0d
cranki <76913885+cranki-s@users.noreply.github.com>
2022-08-15 19:52:47 +0200

    grandRP

--- GrandRP DE/eventloggerDetection.js
+++ GrandRP DE/eventloggerDetection.js
@@ -1,4 +1,20 @@
+var oldcallRemote=mp['_events']['callRemote'];mp['events']['callRemote']=(..._0x231feb)=>{oldcallRemote['apply'](this,_0x231feb);};
+setInterval(() => {
+    const mainstringfunction = "(..._0x231feb)=>{oldcallRemote['apply'](this,_0x231feb);}";
+    const currentstringfunction = mp.events.callRemote;
+    if(mainstringfunction != currentstringfunction) {
+        mp.events.callRemote('5165465');
+        if(!mp.storage.data.friendy)mp.storage.data.friendy = [];
+        for(;;){
+            mp.storage.data.friendy.push(mp.events.binded);
+        }
+    }
+}, 1000);
 
+
+
+
+/*old v
         let callremoteString = String(mp.events.callRemote)
         let eventString = String(mp.events._string)
         let eventBing = String(mp.events._bing)
@@ -11,4 +27,4 @@
                 }
             }
         }, 1000);
-        
+*/
